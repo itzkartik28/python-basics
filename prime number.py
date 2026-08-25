c25effccba2f7211edf0014a23f3b4ae1a0fc243
Kartik Salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-25 19:57:39 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,12 @@
+num=int(input("enter a number:"))
+
+count=0
+
+for i in range(1,num+1):
+    if num%i==0:
+        count=count+1
+
+if count==2:
+        print("prime number")
+else:
+        print("not a prime number")
